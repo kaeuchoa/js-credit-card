eada8b4ee1e242370426f0a006cefb2e28057254
Kaê Uchôa <kaeuchoa@hotmail.com>
2020-03-03 09:36:55 -0300

    adds basic initialization structure to app and card factory

---
+++ app.js
@@ -0,0 +1,36 @@
+const cardFactory = (function(){
+    const _card = {};
+
+    function initCard(cardElement) {
+        _card.element = cardElement;
+        _card.element.addEventListener('click', flipCard);
+        _card.back = cardElement.querySelector(".js-card-back");
+        _card.front = cardElement.querySelector(".js-card-front");
+        return _card;
+    }
+
+    function flipCard(){
+        console.log(_card);
+    }
+
+    return {
+        flipCard: flipCard,
+        init: initCard
+    }
+})();
+
+
+const app = (function(){
+    let _cards = [];
+    function initApp() {
+        let cardsElements = Array.from(document.querySelectorAll(".js-card"));
+        _cards = cardsElements.map(cardElement => cardFactory.init(cardElement));
+    }
+
+    return {
+        init: initApp
+    }
+})(cardFactory);
+
+
+app.init();
