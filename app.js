9a15f1262f9b4e5b5dee0ceb04ffc6f8f900f2da
Kaê Uchôa <kaeuchoa@hotmail.com>
2020-03-10 09:50:55 -0300

    updates card view structure and creates function to bind view to model. Binds card name and holder inputs

--- app.js
+++ app.js
@@ -1,36 +1,63 @@
-const cardFactory = (function(){
-    const _card = {};
+const CardView = (function(){
+    let _view = {
+        init: function() {
+            this.element = document.querySelector(".js-card");
+            this.element.addEventListener('click', flipCard);
+            return this;
+        },
 
-    function initCard(cardElement) {
-        _card.element = cardElement;
-        _card.element.addEventListener('click', flipCard);
-        _card.back = cardElement.querySelector(".js-card-back");
-        _card.front = cardElement.querySelector(".js-card-front");
-        return _card;
-    }
+        bindModel: function (cardModel) {
+            let view = this;
+            return new Proxy(cardModel, {
+                set(target, property, value) {
+                    target[property] = value;
+                    let field = view.element.querySelector(`[data-input-bind="${property}"]`);
+                    field.innerHTML = value;
+                    return true;
+                }
+            });
+        }
+
+    };
 
     function flipCard(){
-        _card.element.classList.toggle('is-flipped');
+        _view.element.classList.toggle('is-flipped');
     }
 
-    return {
-        flipCard: flipCard,
-        init: initCard
-    }
+    return _view;
 })();
 
 
 const app = (function(){
-    let _cards = [];
+    let card = {
+        number: "####-####-####-####",
+        holder: "",
+        securityNumber: "",
+        expirationDate: "MM/YYYY"
+    };
+
+    let cardView = {};
+
     function initApp() {
-        let cardsElements = Array.from(document.querySelectorAll(".js-card"));
-        _cards = cardsElements.map(cardElement => cardFactory.init(cardElement));
+        cardView = CardView.init();
+        card = cardView.bindModel(card);
+
+        let inputNumber = document.querySelector("#card-number");
+        inputNumber.addEventListener("keyup", function(){
+            card.number = inputNumber.value;
+        });
+
+        let inputHolder = document.querySelector("#card-holder");
+        inputHolder.addEventListener("keyup", function(){
+            card.holder = inputHolder.value;
+        });
+
     }
 
     return {
         init: initApp
     }
-})(cardFactory);
+})(CardView);
 
 
 app.init();
